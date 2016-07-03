dc9283348920b26707b243188477b1a1b6d4177c
Lajos Tornyi <ltornyi@gmail.com>
2016-07-03 19:33:34 +0200

    POST, PUT methods added

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,10 +1,10 @@
 var express = require('express'),
     router = express.Router();
-    
+
 var findAll = function(req, res) {
     var db = req.app.get('db');
     db.test1.authors.find({}, function(err, result) {
-        res.send(result);    
+        res.send(result);
     });
 }
 
@@ -15,17 +15,52 @@ var findById = function(req, res) {
         db.test1.authors.findOne(id, function(err, author) {
             if (!author){
                 res.status('404');
-            } 
+            }
             res.send(author);
         });
     }
     else {
         res.status('404').send('');
     }
-    
+
+}
+
+var add = function(req, res) {
+  var db = req.app.get('db');
+  var a = req.body;
+  if (a.hasOwnProperty('author_id')) {
+    //PK specified but this is the insert function
+    delete a.author_id;
+  }
+  db.test1.authors.save(a, function(err,inserted){
+    if (err) {
+      res.send({'error':'An error has occurred', 'text':err});
+    } else {
+      res.status('201');
+      res.setHeader("Location","/authors/" + inserted.author_id);
+      res.send(inserted);
+    }
+  })
+}
+
+var update = function(req, res) {
+  var db = req.app.get('db');
+  var a = req.body;
+  var id = Number(req.params.id);
+  a.author_id = id;
+  db.test1.authors.save(a, function(err,updated) {
+    if (err) {
+      res.send({'error':'An error has occurred', 'text':err});
+    } else {
+      res.status('200');
+      res.send(updated);
+    }
+  })
 }
 
 router.get('/', findAll);
 router.get('/:id', findById);
+router.post('/', add);
+router.put('/:id', update);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 var massive = require('massive');
 var dbConfig = require('epa').getEnvironment()._config;
 var controllers = require('./controllers');
 
 var app = express();
+app.use( morgan('dev'), bodyParser.json());
 
 //db
 var massiveInstance = massive.connectSync(dbConfig);
@@ -15,4 +18,3 @@ app.use(controllers);
 app.listen(3000, function(){
     console.log('Listening on port 3000...');
 });
-
